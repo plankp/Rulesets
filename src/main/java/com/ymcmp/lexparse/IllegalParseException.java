3bad69953362b3dca3e5fa5f02abc50900c73590
Paul Teng <plankp@outlook.com>
2018-06-21 09:37:05 -0400

    Apply license header

--- src/main/java/com/ymcmp/lexparse/IllegalParseException.java
+++ src/main/java/com/ymcmp/lexparse/IllegalParseException.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) 2018 Paul Teng <plankp@outlook.com>.
+ * Licensed under the BSD-3-Clause License - https://raw.githubusercontent.com/plankp/Rulesets/blob/master/LICENSE
+ */
+
 package com.ymcmp.lexparse;
 
 public class IllegalParseException extends RuntimeException {
